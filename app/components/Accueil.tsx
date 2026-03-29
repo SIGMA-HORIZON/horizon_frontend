77041093ee26bd6e5785715033fed33a5ab63c75
Negou  Donald <tameguedonald@gmail.com>
2026-03-29 11:31:07 +0100

    ajout des pages

--- app/components/Accueil.tsx
+++ app/components/Accueil.tsx
@@ -9,10 +9,10 @@ const Accueil = () => {
       <div className="pt-20 flex items-center justify-center display-flex space-x-4">
 
         <div>   page d'accueil Horizon <br /></div> <br />
-        <div> <button>  <a href="connexion">Connexion</a> </button></div>
-        <div> <button>  <a href="dashboad-admin">dashboad</a> </button></div>
-        <div> <button>  <a href="mon-profil-use">profil</a> </button></div>
-        <div> <button>  <a href="mes-vms"> liste vms</a> </button></div>
+        <div> <button className="bg-blue-400 ">  <a href="connexion">Connexion</a> </button></div>
+        <div> <button className="bg-blue-400 ">  <a href="dashboad-admin">dashboad</a> </button></div>
+        <div> <button className="bg-blue-400 ">  <a href="mon-profil-use">profil</a> </button></div>
+        <div> <button className="bg-blue-400 ">  <a href="mes-vms"> liste vms</a> </button></div>
       </div>
     </section>
   );
