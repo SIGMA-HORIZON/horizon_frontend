a4cfeab32cfd9f1e58c9c3a92fe5915f43c7c0fa
Negou  Donald <tameguedonald@gmail.com>
2026-04-25 21:57:10 +0100

    console

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@novnc/novnc'],
-  experimental: {
-    esmExternals: 'loose',
-  },
   webpack: (config) => {
-    // Top-level await is needed for some ESM features
     config.experiments = { ...config.experiments, topLevelAwait: true };
-    
+
+    config.module.rules.push({
+      test: /@novnc\/novnc\/.*\.js$/,
+      parser: {
+        topLevelAwait: true,
+      },
+    });
+
     return config;
   },
 };
